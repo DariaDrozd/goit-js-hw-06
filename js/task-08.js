ee1f5e05f37588bb2ef6fcdd3bfc32a78a08b634
DariaDrozd <reddymargot@gmail.com>
2023-02-25 00:42:36 +0200

    final 9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener('submit', formSumb);
+
+function formSumb (event) {
+    event.preventDefault();
+
+    const formElem = event.currentTarget.elements;
+    const email = formElem.email.value;
+    const password = formElem.password.value;
+
+
+    const formInfo = {
+        email,
+        password,
+    };
+
+    if (email === "" || password === "") {
+        alert('Complet all form fields!');
+
+    } else {
+        console.log(formInfo);
+    };
+
+    loginForm.reset();
+};
--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,23 @@
+//Напиши скрипт, який змінює кольори фону елемента <body> 
+//через інлайн - стиль по кліку на button.change - color і виводить значення кольору в span.color.
+
+
+
+
+const color = document.querySelector('.widget');
+const button = document.querySelector('.change-color');
+const span = document.querySelector('.color');
+
+button.addEventListener('click', event => {
+  color.style.color = getRandomHexColor();
+  span.textContent = color.style.color;
+
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
+
+
